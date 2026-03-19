5d31b5be2576f37de32af3d5f5df07fb17da63a5
nikita <nikitagmyryak@gmail.com>
2026-03-19 16:40:07 +0100

    added User Repo class

--- src/main/java/com/demo/messanger/repositories/UserRepository.java
+++ src/main/java/com/demo/messanger/repositories/UserRepository.java
@@ -1,4 +1,13 @@
 package com.demo.messanger.repositories;
 
-public class UserRepository {
+import com.demo.messanger.models.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import java.util.Optional;
+
+@Repository
+public interface UserRepository extends JpaRepository<User,Long>{
+    Optional<User> findById(Long id);
+    Optional<User> findByEmail(String email);
+    Optional<User> findByUsername(String username);
 }
