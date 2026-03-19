db78ad98b7c138d2a02217270102d4b0df7b37dc
unknown <you@example.com>
2026-03-19 17:10:00 +0100

    MessageService commit

--- src/main/java/com/demo/messanger/validations/MessageValidator.java
+++ src/main/java/com/demo/messanger/validations/MessageValidator.java
@@ -1,4 +1,16 @@
 package com.demo.messanger.validations;
 
+import com.demo.messanger.models.Message;
+
 public class MessageValidator {
+
+
+    public boolean checkMessage(Message message){
+        if(!(message.getMessage().isBlank())){
+            message.setMessage(message.getMessage().trim());
+            return true;
+        }else{
+            return false;
+        }
+    }
 }
